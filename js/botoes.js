632ca03892c598ee955641367c360e8561a9d54d
Luiz Henrique Zavatini Feltrin <106353903+BillNobill@users.noreply.github.com>
2022-11-14 20:00:33 -0300

    Update botoes.js

--- js/botoes.js
+++ js/botoes.js
@@ -1,7 +1,1 @@
 
-function deletarTexto() {
-    document.getElementById('textoPrincipal').innerHTML = ' ';
-    document.getElementById('textoTitulo').innerHTML = 'Título*';
-    document.getElementById('textoNome').innerHTML = 'Nome*';
-    document.getElementById('textoData').innerHTML = 'Data*';
-}
